867631af2813a34bf24c2c96d854d2869f04ccec
Dewi.x0 <danbat2006@gmail.com>
2025-09-21 14:12:07 +0300

    added backend structure

---
+++ backend/env_baceknd.md
@@ -0,0 +1,14 @@
+# Redis Configuration
+REDIS_PASS=redispassword
+REDIS_HOST=localhost
+REDIS_PORT=6379
+REDIS_DB=0
+
+# Database Configuration
+DB_NAME=dbname
+DB_USER=dbuser
+DB_PASSWORD=dbpassword
+
+# Database settings based on server config
+DB_HOST=localhost
+DB_PORT=5433
---
+++ backend/src/api/base.py
@@ -0,0 +1,25 @@
+from fastapi import APIRouter, Depends
+from src.utils.dependencies import get_redis, get_db_session
+from src.utils.redis_client import RedisClient
+from src.utils.database import check_postgres_connection
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import text
+
+router=APIRouter(prefix="/base")
+
+@router.get("/health")
+async def health(redis: RedisClient = Depends(get_redis)):
+    """Проверка состояния сервиса(подключение к бд)"""
+    redis_status = redis.ping()
+    
+    postgres_status = await check_postgres_connection()
+    
+    overall_status = "healthy" if redis_status and postgres_status else "unhealthy"
+    
+    return {
+        "status": overall_status,
+        "services": {
+            "redis": "connected" if redis_status else "disconnected",
+            "postgresql": "connected" if postgres_status else "disconnected"
+        }
+    }
